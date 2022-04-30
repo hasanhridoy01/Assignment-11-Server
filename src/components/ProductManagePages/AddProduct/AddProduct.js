fb530790677b685eca02a42883b8d9b6f214c079
hridoy <hasanhridot1367@gmail.com>
2022-04-30 16:28:48 -0700

    add product page done

--- src/components/ProductManagePages/AddProduct/AddProduct.js
+++ src/components/ProductManagePages/AddProduct/AddProduct.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const AddProduct = () => {
   return (
-    <div className='container mt-5 mb-5'>
-      <h3>This is add new product pages</h3>
+    <div className='container mt-5 mb-5 p-5'>
+      <div className="card p-5 w-50 mx-auto shadow">
+       <h3>Add New Product</h3>
+        <Form>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Product Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter Product Name" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Supplier Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter Supplier Name" />
+          </Form.Group>
+          
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Product Price</Form.Label>
+            <Form.Control type="text" placeholder="Enter Product Price" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Product Description</Form.Label>
+            <textarea className='form-control' type="text" placeholder="Enter Product Description" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 };
